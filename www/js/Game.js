fe234850f2123612facea397083a27a668de0fb0
Fengda HUANG <h@phodal.com>
2016-06-01 23:16:48 +0800

    change button layout

--- www/js/Game.js
+++ www/js/Game.js
@@ -6,7 +6,7 @@ Growth.Game = function (game) {
 Growth.Game.prototype = {
 
   create: function () {
-   
+    var text = this.game.add.text(0, 0, "游戏开始中。。。");
   },
 
   gameOver: function () {
--- www/js/MainMenu.js
+++ www/js/MainMenu.js
@@ -4,13 +4,17 @@ Growth.MainMenu = function (game) {
 
 Growth.MainMenu.prototype = {
   create: function () {
-    var text = this.game.add.text(20, 20, "Start");
-    var hitRect = new Phaser.Rectangle(15, 10, 300, 50);
+    var text = this.game.add.text(this.game.world.centerX - 50, this.game.world.centerY - 15, "开始游戏");
 
-    text.hitArea = hitRect;
     text.inputEnabled = true;
+    text.fill = "#fff";
     text.buttonMode = true;
     text.events.onInputDown.add(this.startGame, this);
+
+    var graphics = this.game.add.graphics(0, 0);
+    // draw a rectangle
+    graphics.lineStyle(2, 0xffffff, 1);
+    graphics.drawRoundedRect(this.game.world.centerX - 100, this.game.world.centerY - 20, 200, 40, 6);
   },
 
   update: function () {
