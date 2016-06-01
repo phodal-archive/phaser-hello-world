2b842f6a1e9282695d33a97b9a668ab11dd8504f
Fengda HUANG <h@phodal.com>
2016-06-01 23:22:46 +0800

    add button background

--- www/js/MainMenu.js
+++ www/js/MainMenu.js
@@ -4,6 +4,12 @@ Growth.MainMenu = function (game) {
 
 Growth.MainMenu.prototype = {
   create: function () {
+    var graphics = this.game.add.graphics(0, 0);
+    // draw a rectangle
+    graphics.beginFill(0xdddddd);
+    graphics.lineStyle(2, 0xffffff, 1);
+    graphics.drawRoundedRect(this.game.world.centerX - 100, this.game.world.centerY - 20, 200, 40, 6);
+
     var text = this.game.add.text(this.game.world.centerX - 50, this.game.world.centerY - 15, "开始游戏");
 
     text.inputEnabled = true;
@@ -11,10 +17,6 @@ Growth.MainMenu.prototype = {
     text.buttonMode = true;
     text.events.onInputDown.add(this.startGame, this);
 
-    var graphics = this.game.add.graphics(0, 0);
-    // draw a rectangle
-    graphics.lineStyle(2, 0xffffff, 1);
-    graphics.drawRoundedRect(this.game.world.centerX - 100, this.game.world.centerY - 20, 200, 40, 6);
   },
 
   update: function () {
